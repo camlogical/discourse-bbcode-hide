86e36d391de189fb9b7a12c8d1af771591a91926
Hai Trieu To <trieuth@office.edu.vn>
2019-08-19 22:51:03 +0700

    Create pretty_text_spec.rb

---
+++ pretty_text_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe PrettyText do
+  it 'can apply hide bbcode' do
+    cooked = PrettyText.cook "hello [hide=guest]Hide content[/hide] world"
+    html = '<p>hello <div class="hide guest">Hide Content</div> world</p>'
+
+    expect(cooked).to eq(html)
+  end
+end
