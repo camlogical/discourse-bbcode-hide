7d85463d3af78af30afcabd87c9c4d4e63eac9fb
Hai Trieu To <trieuth@office.edu.vn>
2019-08-20 00:52:56 +0700

    Update bbcode_hide_dialect.js

--- assets/javascripts/bbcode_hide_dialect.js
+++ assets/javascripts/bbcode_hide_dialect.js
@@ -1,10 +1,10 @@
 (function() {
   function replaceFontBgColor (text) {
       while (text !== (text = text.replace(/\[hide=([^\]]+)\]((?:(?!\[hide=[^\]]+\]|\[\/hide\])[\S\s])*)\[\/hide\]/ig, function (match, p1, p2) {
-        return "<div class='hide " + p1 + "'>" + p2 + "</div>";
+        return "<div class='hideto " + p1 + "'>" + p2 + "</div>";
       })));
       return text;
     }
     Discourse.Dialect.addPreProcessor(replaceContentHide);
-    Discourse.Markdown.whiteListTag('div', 'class', /^hide .*$/i);
+    Discourse.Markdown.whiteListTag('div', 'class', /^hideto .*$/i);
 })();
