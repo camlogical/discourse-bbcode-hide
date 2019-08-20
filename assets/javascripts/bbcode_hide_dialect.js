7eac5f601a602781a1ef3252fe0458dfb4ddc475
Hai Trieu To <trieuth@office.edu.vn>
2019-08-21 01:22:56 +0700

    Update bbcode_hide_dialect.js

--- assets/javascripts/bbcode_hide_dialect.js
+++ assets/javascripts/bbcode_hide_dialect.js
@@ -1,10 +1,3 @@
 (function() {
-  function replaceContentHide (text) {
-      while (text !== (text = text.replace(/\[hideto=([^\]]+)\]((?:(?!\[hideto=[^\]]+\]|\[\/hideto\])[\S\s])*)\[\/hideto\]/ig, function (match, p1, p2) {
-        return "<div class='hideto " + p1 + "'>" + p2 + "</div>";
-      })));
-      return text;
-    }
-    Discourse.Dialect.addPreProcessor(replaceContentHide);
-    Discourse.Markdown.whiteListTag('div', 'class', /^hideto .*$/i);
+    Discourse.Markdown.whiteListTag('div', 'class', '*');
 })();
